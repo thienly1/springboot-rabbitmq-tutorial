76ce24d502741a216a1711764c3fbcd756364cb4
THI LY TA <92649607+thienly1@users.noreply.github.com>
2023-05-28 04:41:39 +0200

    Configure RabbitMQ in Springboot Application

---
+++ src/main/java/net/javamicro/springboot/config/RabbitMQConfig.java
@@ -0,0 +1,43 @@
+package net.javamicro.springboot.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    //Spring bean for RabbitMQ queue
+    @Bean
+    public Queue queue(){
+        return new Queue(queue);
+    }
+    //Spring bean for rabbitmq exchange
+    @Bean
+    public TopicExchange exchange(){
+        return new TopicExchange(exchange);
+    }
+    //Binding between queue and exchange using routing key
+    @Bean
+    public Binding binding(){
+        return BindingBuilder
+                .bind(queue())
+                .to(exchange())
+                .with(routingKey);
+    }
+    //springboot application will automatically configure these bean, just create an inject then you can use these bean
+    //ConnectionFactory
+    //RabbitTemplate
+    //RabbitAdmin
+
+}
