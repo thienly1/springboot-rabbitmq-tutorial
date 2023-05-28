291404d4c547425ae28762249c57fea5c3951b5f
THI LY TA <92649607+thienly1@users.noreply.github.com>
2023-05-28 05:14:28 +0200

    Create Producer and Consumer to producer and receive message

---
+++ src/main/java/net/javamicro/springboot/controller/MessageController.java
@@ -0,0 +1,26 @@
+package net.javamicro.springboot.controller;
+
+import net.javamicro.springboot.publisher.RabbitMQProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MessageController {
+
+    private RabbitMQProducer rabbitMQProducer;
+
+    public MessageController(RabbitMQProducer rabbitMQProducer) {
+        this.rabbitMQProducer = rabbitMQProducer;
+    }
+
+    //http://localhost:8080/api/v1/publish?message=hello world
+    @GetMapping("/publish")
+    public ResponseEntity<String> sendMessage(@RequestParam("message") String message){
+        rabbitMQProducer.sendMessage(message);
+        return ResponseEntity.ok("Message sent to RabbitMQ....");
+    }
+}
